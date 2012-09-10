89eceeea630cd1bbd08077d4b82c0221b5dc2c9a
Vincent Driessen <vincent@3rdcloud.com>
2012-09-10 11:03:23 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,55 @@
+"""
+pip-refresh keeps your Python package dependencies fresh, yet pinned down.
+"""
+import sys
+import os
+from setuptools import setup, find_packages
+
+
+def get_version():
+    basedir = os.path.dirname(__file__)
+    with open(os.path.join(basedir, 'pip_refresh/version.py')) as f:
+        VERSION = None
+        exec(f.read())
+        return VERSION
+    raise RuntimeError('No version info found.')
+
+
+setup(
+    name='pip-refresh',
+    version=get_version(),
+    url='https://github.com/nvie/pip-refresh/',
+    license='BSD',
+    author='Vincent Driessen',
+    author_email='vincent@3rdcloud.com',
+    description='pip-refresh keeps your Python package dependencies fresh, yet '
+            'pinned down.',
+    #long_description='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['pip'],
+    entry_points='''\
+    [console_scripts]
+    pip-refresh = pip_refresh.scripts.pip_refresh:main
+    ''',
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        #'Development Status :: 1 - Planning',
+        #'Development Status :: 2 - Pre-Alpha',
+        #'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 6 - Mature',
+        #'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: Utilities',
+    ]
+)
