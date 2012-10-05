ad6144500b386c78d6883058a2ff0268626bcaf0
Vincent Driessen <vincent@3rdcloud.com>
2012-10-05 16:09:25 +0200

    This implements the FakePackageManager.
    Among changes, this adds the version module, which is a copy of
    distutils2.version, for PEP386 compatible version comparison.
    (Straight-including this simple module avoids adding a distutils2
    dependency to pip-tools.)
    
    Test cases for the FakePackageManager itself are also included.

--- piptools/package_manager.py
+++ piptools/package_manager.py
@@ -1,3 +1,9 @@
+import operator
+from functools import partial
+from version import NormalizedVersion  # PEP386 compatible version numbers
+from datastructures import Spec
+
+
 class NoPackageMatch(Exception):
     pass
 
@@ -11,7 +17,74 @@ class BasePackageManager(object):
 
 
 class FakePackageManager(BasePackageManager):
-    pass
+    def __init__(self, fake_contents):
+        """Creates a fake package manager index, for easy testing.  The
+        fake_contents argument is a dictionary containing 'name-version' keys
+        and lists-of-specs values.
+
+        Example:
+
+            {
+                'foo-0.1': ['bar', 'qux'],
+                'bar-0.2': ['qux>0.1'],
+                'qux-0.1': [],
+                'qux-0.2': [],
+            }
+        """
+        # Sanity check (parsing will return errors if content is wrongly
+        # formatted)
+        for pkg_key in fake_contents:
+            self.parse_package_key(pkg_key)
+
+        self._contents = fake_contents
+
+    def parse_package_key(self, pkg_key):
+        try:
+            return pkg_key.rsplit('-', 1)
+        except ValueError:
+            raise ValueError('Invalid package key: %s (required format: "name-version")' % (pkg_key,))
+
+    def iter_package_versions(self):
+        """Iters over all package versions, returning key-value pairs."""
+        for key in self._contents:
+            yield self.parse_package_key(key)
+
+    def iter_versions(self, given_name):
+        """Will return all versions available for the current package name."""
+        for name, version in self.iter_package_versions():
+            if name == given_name:
+                yield version
+
+    def matches_qual(self, version, qual, value):
+        """Returns whether version matches the qualifier and the given value."""
+        ops = {
+            '==': operator.eq,
+            '<': operator.lt,
+            '>': operator.gt,
+            '<=': operator.le,
+            '>=': operator.ge,
+        }
+        return ops[qual](NormalizedVersion(version), NormalizedVersion(value))
+
+    def pick_highest(self, list_of_versions):
+        """Picks the highest version from a list, according to PEP386 logic."""
+        return str(max(map(NormalizedVersion, list_of_versions)))
+
+    def find_best_match(self, spec):
+        """This requires a bit of reverse engineering of PyPI's logic that
+        finds a pacakge for a given spec, but it's not too hard.
+        """
+        versions = list(self.iter_versions(spec.name))
+        for qual, value in spec.specs:
+            pred = partial(self.matches_qual, qual=qual, value=value)
+            versions = filter(pred, versions)
+        if len(versions) == 0:
+            raise NoPackageMatch('No package found for %s' % (spec,))
+        return self.pick_highest(versions)
+
+    def get_dependencies(self, name, version):
+        pkg_key = '%s-%s' % (name, version)
+        return Spec.from_line(self._contents[pkg_key])
 
 
 class PackageManager(BasePackageManager):
