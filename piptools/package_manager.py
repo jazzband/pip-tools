d1cd0b041be877fdcc3c81eb10405e394244aab9
Vincent Driessen <vincent@3rdcloud.com>
2012-10-05 16:05:47 +0200

    Commit base layout for the package_manager.py file.

---
+++ piptools/package_manager.py
@@ -0,0 +1,23 @@
+class NoPackageMatch(Exception):
+    pass
+
+
+class PackageManager(object):
+    def find_best_match(self, spec):
+        raise NotImplementedError('Implement this in a subclass.')
+
+    def get_dependencies(self, name, version):
+        raise NotImplementedError('Implement this in a subclass.')
+
+
+class FakePackageManager(BasePackageManager):
+    pass
+
+
+class PackageManager(BasePackageManager):
+    """The default package manager that goes to PyPI and caches locally."""
+    pass
+
+
+if __name__ == '__main__':
+    pass
