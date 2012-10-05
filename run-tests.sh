fc2dc07d9a32a8b6be023882db8fd88fd9070a84
Vincent Driessen <vincent@3rdcloud.com>
2012-10-05 14:44:08 +0200

    Add location for unit tests.

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+set -e
+echo "===> Running unit tests"
+python -m unittest discover -s tests/unit
+echo
+echo "===> Running cram tests"
 cram tests/*.t
---
+++ tests/unit/test_spec_set.py
@@ -0,0 +1,5 @@
+import unittest
+
+
+class TestSpecSet(unittest.TestCase):
+    pass
