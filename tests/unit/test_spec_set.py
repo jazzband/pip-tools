8fba76340daef349c45946183757ef463004492b
Vincent Driessen <vincent@3rdcloud.com>
2012-10-05 20:35:59 +0200

    Add test cases for testing SpecSet.

--- tests/unit/test_spec_set.py
+++ tests/unit/test_spec_set.py
@@ -1,5 +1,34 @@
 import unittest
+from piptools.datastructures import SpecSet, Spec
 
 
 class TestSpecSet(unittest.TestCase):
-    pass
+    def test_adding_specs(self):
+        """Adding specs to a set."""
+        specset = SpecSet()
+
+        specset.add_spec(Spec.from_line('Django>=1.3'))
+        assert 'Django>=1.3' in map(str, specset)
+
+        specset.add_spec(Spec.from_line('django-pipeline'))
+        self.assertItemsEqual(['Django>=1.3', 'django-pipeline'], map(str, specset))
+
+        specset.add_spec(Spec.from_line('Django<1.4'))
+        self.assertItemsEqual(['Django>=1.3', 'django-pipeline', 'Django<1.4'], map(str, specset))
+
+    def test_normalizing(self):
+        """Normalizing combines predicates to a single Spec."""
+        specset = SpecSet()
+
+        specset.add_spec(Spec.from_line('Django>=1.3'))
+        specset.add_spec(Spec.from_line('Django<1.4'))
+        specset.add_spec(Spec.from_line('Django>=1.3.2'))
+        specset.add_spec(Spec.from_line('Django<1.3.99'))
+
+        normalized = specset.normalize()
+        assert 'Django>=1.3.2,<1.3.99' in map(str, normalized)
+
+        specset.add_spec(Spec.from_line('Django<=1.3.2'))
+        normalized = specset.normalize()
+
+        assert 'Django==1.3.2' in map(str, normalized)
