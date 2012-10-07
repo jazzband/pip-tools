f32d9606d9f3d62e440bedfdd9600c7879d066b7
Vincent Driessen <vincent@3rdcloud.com>
2012-10-07 14:48:54 +0200

    Remove complex example.
    Decided with @brutasse that we won't support this case automatically,
    and instead error with a helpful (!) conflict message so users can
    manually add extra specs to the requirements.in file, to hint about
    a possible solution.

--- tests/unit/test_spec_resolving.py
+++ tests/unit/test_spec_resolving.py
@@ -19,42 +19,6 @@ simple = {
 }
 
 
-# The following example _does_ have a solution, but the algorithm doesn't find
-# it.  If we're able to smartly backtrack and be able to select qux-0.1, we
-# solve it.  My basic idea for the backtracking:
-#
-# When we encounter a conflict in a round with a newly added package, like
-# when we add 'simplejson<2.6 (from qux==0.2)', which conflicts with the
-# existing 'simplejson>=2.6 (from foo==0.1)', then we remove the source
-# package for that from the spec set.  In our case, this would be qux==0.2.
-# And instead, we add a fake spec 'qux<0.2 (from conflict resolution
-# attempt)' and continue from there.
-#
-# I like the idea above, but I'm worried about some details:
-# - How complex will this make real-life situations with many dependencies?
-#   In the case above, only qux conflicts, but in reality multiple packages
-#   may cause conflicts.  Theoretically, any combination of packages should be
-#   tried, potentially causing an explosion of conflict resolution attempts.
-# - Also, this can potentially try out each version of a package (until there
-#   is a conflict or we run out of versions).  This, of course, is exactly the
-#   point, but in practice, it's not likely that you'll find anything useful
-#   with this technique.
-#
-# Alternatively, we can stop trying to solve this programatically, and instead
-# output some useful information so the user at the wheel can guide the
-# process and explicitly add the conflict resolution spec in his/her
-# requirements.in manually.
-#
-# Thoughts?
-#
-complex = {
-    'foo-0.1': ['bar'],
-    'bar-1.2': ['qux', 'simplejson>=2.6'],
-    'qux-0.1': ['simplejson'],
-    'qux-0.2': ['simplejson<2.6'],
-
-    'simplejson-2.4.0': [],
-    'simplejson-2.6.2': [],
 }
 
 
