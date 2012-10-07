6c22c5c255deb8a22cf844c9c62eabce4769f9e1
Vincent Driessen <vincent@3rdcloud.com>
2012-10-07 16:47:18 +0200

    Move core of the algo to Resolver class.
    The Resolver class now lives under piptools.resolver module.

---
+++ piptools/resolver.py
@@ -0,0 +1,95 @@
+from piptools.datastructures import SpecSet, Spec
+
+
+def print_specset(specset, round):
+    print('After round #%s:' % (round,))
+    for spec in specset:
+        print('  - %s' % (spec.description(),))
+
+
+class Resolver(object):
+    def __init__(self, spec_set, package_manager=None):
+        """This class resolves a given SpecSet by querying the given
+        PackageManager.
+        """
+        # TODO: If no specific PackageManager instance is used, use the
+        # default package manager (locally cached PyPI).
+        self.spec_set = spec_set
+        #if package_manager is None:
+        #    from piptools.package_manager import PackageManager
+        #    package_manager = PackageManager(...)
+        self.pkgmgr = package_manager
+
+    def resolve_one_round(self):
+        """Resolves one level of the current spec set, by finding best matches
+        for the current spec set in the package manager and returning all
+        (new) requirements for those packages.
+
+        Returns whether the spec set was changed significantly by this round.
+        """
+        new_deps = self.find_new_dependencies()
+        self.spec_set.add_specs(new_deps)
+        return len(new_deps) > 0
+
+    def resolve(self, max_rounds=12):
+        """Resolves the spec set one round at a time, until the set does not
+        change significantly anymore.  Protects against infinite loops by
+        breaking out after a max number rounds.
+        """
+        round = 0
+        while True:
+            round += 1
+            if round > max_rounds:
+                raise RuntimeError('Spec set was not resolved after %d rounds. '
+                        'This is likely a bug.' % max_rounds)
+
+            if not self.resolve_one_round():
+                # Break as soon as nothing significant is added in this round
+                break
+
+            print 'After round #%d' % round
+            print_specset(self.spec_set, round)
+
+        # Return the pinned spec set
+        return self.pin_spec_set()
+
+    def pin_spec_set(self):
+        """Pins all packages in given resolved spec set and returns a new spec
+        set.  Requires the input spec set to be resolved.
+        """
+        new_spec_set = SpecSet()
+        for spec in self.spec_set.normalize():
+            best_version = self.pkgmgr.find_best_match(spec)
+            pinned_spec = Spec(spec.name, [('==', best_version)], spec.source)
+            new_spec_set.add_spec(pinned_spec)
+        return new_spec_set
+
+    def find_all_dependencies(self):
+        """Finds best matches for the current spec set in the package manager,
+        returning all requirements for those packages.
+        """
+        spec_set = self.spec_set
+        pkgmgr = self.pkgmgr
+
+        deps = set()
+        for spec in spec_set.normalize():
+            version = pkgmgr.find_best_match(spec)
+            pkg_deps = pkgmgr.get_dependencies(spec.name, version)
+
+            # Append source information to the new specs
+            if spec.source:
+                source = '%s ~> %s==%s' % (spec.source, spec.name, version)
+            else:
+                source = '%s==%s' % (spec.name, version)
+
+            pkg_deps = {s.add_source(source) for s in pkg_deps}
+            deps.update(pkg_deps)
+
+        return deps
+
+    def find_new_dependencies(self):
+        """Finds all dependencies for the given spec set (in the package
+        manager), but only returns what specs are new to the set.
+        """
+        all_deps = self.find_all_dependencies()
+        return all_deps - set(self.spec_set)  # only return _new_ specs
--- tests/unit/test_spec_resolving.py
+++ tests/unit/test_spec_resolving.py
@@ -1,12 +1,7 @@
 import unittest
 from piptools.datastructures import SpecSet
+from piptools.resolver import Resolver
 from piptools.package_manager import FakePackageManager
-
-
-def print_specset(specset, round):
-    print('After round #%s:' % (round,))
-    for spec in specset:
-        print('  - %s' % (spec.description(),))
 
 
 simple = {
@@ -19,48 +14,16 @@ simple = {
 }
 
 
+
+
 class TestDependencyResolving(unittest.TestCase):
     def test_find_dependencies_simple(self):
         """A simple scenario for finding dependencies."""
         pkgmgr = FakePackageManager(simple)
 
         spec_set = SpecSet()
-        spec_set.add_spec('foo')
+        spec_set.add_spec('sentry')
 
-        round = 1
-        print_specset(spec_set, round)
-        while True:
-            round += 1
-            new_deps = []
-            for spec in spec_set.normalize():
-                version = pkgmgr.find_best_match(spec)
-                pkg_deps = pkgmgr.get_dependencies(spec.name, version)
-
-                # Append source information to the new specs
-                if spec.source:
-                    source = '%s ~> %s==%s' % (spec.source, spec.name, version)
-                else:
-                    source = '%s==%s' % (spec.name, version)
-                pkg_deps = [s.add_source(source) for s in pkg_deps]
-                new_deps += pkg_deps
-
-            if not new_deps:
-                break
-
-            # TODO: We should detect whether adding the new_deps really
-            # "changes anything" to the spec set.  In order words: if no
-            # significant new constraints are added, we're done
-
-            # XXX: FIXME: Current, we "just stop" after X rounds (to prevent
-            # endless loops), but obviously this is not the correct impl!
-            if round > 4:
-                break
-
-            spec_set.add_specs(new_deps)
-            print_specset(spec_set, round)
-
-        # Print the final result:
-        print_specset(spec_set.normalize(), 'final')
-
-        spec_set = spec_set.normalize()
-        self.assertItemsEqual(['foo', 'qux', 'bar', 'simplejson<2.6'], map(str, spec_set))
+        resolver = Resolver(spec_set, pkgmgr)
+        print '-' * 80
+        print resolver.resolve()
