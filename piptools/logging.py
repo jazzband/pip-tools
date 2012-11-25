e7108ffcbc5b54ea51a74ee80b0a92583d92ce33
Bruno Renié <buburno@gmail.com>
2012-11-25 10:01:13 +0100

    pip-compile now complete.
    Introduced a way to silence the logger since we're using the resolver twice.
    This is suboptimal since it requires unpacking archives multiple time but can
    be memoized easily.

--- piptools/logging.py
+++ piptools/logging.py
@@ -33,4 +33,39 @@ class IndentingLogger(Logger):
         return IndentationContext(self)
 
 
-logger = IndentingLogger('piptools')
+class QuietContext(object):
+    def __init__(self, logger):
+        self._logger = logger
+
+    def __enter__(self):
+        self._logger.do_silent()
+
+    def __exit__(self, type, value, traceback):
+        self._logger.do_unsilent()
+
+
+class QuietLogger(Logger):
+    def __init__(self, *args, **kwargs):
+        super(QuietLogger, self).__init__(*args, **kwargs)
+        self._quiet = False
+
+    def _log(self, level, msg, *args, **kwargs):
+        if self._quiet:
+            return
+        super(QuietLogger, self)._log(level, msg, *args, **kwargs)
+
+    def do_silent(self):
+        self._quiet = True
+
+    def do_unsilent(self):
+        self._quiet = False
+
+    def silent(self):
+        return QuietContext(self)
+
+
+class PipToolsLogger(QuietLogger, IndentingLogger):
+    pass
+
+
+logger = PipToolsLogger('piptools')
