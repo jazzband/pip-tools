e019f58dae9a24e3d463e43d044f305c61b96a66
Vincent Driessen <vincent@3rdcloud.com>
2013-03-20 10:14:59 -0700

    Bump version number.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_dependencies():
 
 setup(
     name='pip-tools',
-    version='0.2.1',
+    version='0.3',
     url='https://github.com/nvie/pip-tools/',
     license='BSD',
     author='Vincent Driessen',
