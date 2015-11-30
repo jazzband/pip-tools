ab4cf5e57f24f791cb6842e6a31e896596f9e541
Vincent Driessen <me@nvie.com>
2015-11-30 08:56:32 +0100

    Explicitly declare Python 3.5 support

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         # 'Programming Language :: Python :: 3.2',
         # 'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: BSD License',
