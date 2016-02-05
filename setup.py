acadb4dd2c7d182d20d2813ecdfec33774fd6658
Vincent Driessen <me@nvie.com>
2016-02-05 09:23:43 +0100

    This is 1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pip-tools',
-    version='1.5',
+    version='1.6',
     url='https://github.com/nvie/pip-tools/',
     license='BSD',
     author='Vincent Driessen',
