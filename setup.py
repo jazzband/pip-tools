a52b6e35a2d00316c0c66edda4f186ff6b0d5c4b
Vincent Driessen <me@nvie.com>
2016-11-17 15:52:35 +0100

    Bump to 1.8.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pip-tools',
-    version='1.7.1dev0',
+    version='1.8.0',
     url='https://github.com/nvie/pip-tools/',
     license='BSD',
     author='Vincent Driessen',
