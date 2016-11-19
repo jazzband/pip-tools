939eb457a5885bdcf9b5ee1b53b48f42a32e02bc
Philip Jenvey <pjenvey@underboss.org>
2016-11-18 20:31:44 -0800

    move annotations to the following line w/ --generate-hashes

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -23,7 +23,7 @@ def test_format_requirement_annotation_editable(from_editable, writer):
     assert (writer._format_requirement(ireq,
                                        reverse_dependencies,
                                        primary_packages=[]) ==
-            '-e git+git://fake.org/x/y.git#egg=y' + comment('  # via xyz'))
+            '-e git+git://fake.org/x/y.git#egg=y  ' + comment('# via xyz'))
 
 
 def test_format_requirement_annotation(from_line, writer):
@@ -33,7 +33,7 @@ def test_format_requirement_annotation(from_line, writer):
     assert (writer._format_requirement(ireq,
                                        reverse_dependencies,
                                        primary_packages=[]) ==
-            'test==1.2               ' + comment('  # via xyz'))
+            'test==1.2                 ' + comment('# via xyz'))
 
 
 def test_format_requirement_annotation_case_sensitive(from_line, writer):
@@ -43,7 +43,7 @@ def test_format_requirement_annotation_case_sensitive(from_line, writer):
     assert (writer._format_requirement(ireq,
                                        reverse_dependencies,
                                        primary_packages=[]) ==
-            'Test==1.2               ' + comment('  # via xyz'))
+            'Test==1.2                 ' + comment('# via xyz'))
 
 
 def test_format_requirement_not_for_primary(from_line, writer):
