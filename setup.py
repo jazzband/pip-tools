ad9e5638bf29e8de7ad5ba66feb36e8d2175074b
Julian Gonggrijp <j.gonggrijp@uu.nl>
2017-02-21 14:40:10 +0100

    Remove Python 2.6 support
    The shift to using `subprocess.call[sys.executable, '-m']` is not
    backwards compatible. Python 2.6 could do this trick only with single
    modules, not with packages.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         #'Programming Language :: Python :: 2.3',
         #'Programming Language :: Python :: 2.4',
         #'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
+        #'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         #'Programming Language :: Python :: 3.0',
