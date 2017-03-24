df10e6ab7897bb9c4b6306ebc09a953a01a7cae6
Dylan Lloyd <dylan@disinclined.org>
2017-03-24 14:18:45 -0400

    preserve index_urls order during deduplication

--- piptools/scripts/compile.py
+++ piptools/scripts/compile.py
@@ -6,6 +6,7 @@ import optparse
 import os
 import sys
 import tempfile
+from collections import OrderedDict
 
 import pip
 from pip.req import InstallRequirement, parse_requirements
@@ -144,7 +145,7 @@ def cli(verbose, dry_run, pre, rebuild, find_links, index_url, extra_index_url,
 
     log.debug('Using indexes:')
     # remove duplicate index urls before processing
-    repository.finder.index_urls = sorted(set(repository.finder.index_urls))
+    repository.finder.index_urls = list(OrderedDict.fromkeys(repository.finder.index_urls))
     for index_url in repository.finder.index_urls:
         log.debug('  {}'.format(index_url))
 
