6f85a70d2749100750f93b589e5e4c2a4f56569b
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-06-08 16:06:44 +0300

    tests: Rename "fixtures" to "test_data"
    If pytest is ran with "--doctest-modules" option, then the test dist
    packages under the "fixtures" directory should be ignored, because
    otherwise pytest tries to import the "setup.py" files and fails.
    
    The name "fixtures" suggests that it could also contain some Python code
    that shouldn't be ignored by pytest, but currently it contains only test
    data, such as fake PyPI index, test dist packages and test wheels.
    Rename the "fixtures" directory to "test_data" so that it can be marked
    to be ignored by pytest without risking ignoring too much.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -153,7 +153,7 @@ def test_realistic_complex_sub_dependencies(tmpdir):
     subprocess.check_output(['pip', 'wheel',
                              '--no-deps',
                              '-w', str(tmpdir),
-                             os.path.join('.', 'tests', 'fixtures', 'fake_package', '.')])
+                             os.path.join('.', 'tests', 'test_data', 'fake_package', '.')])
 
     runner = CliRunner()
     with runner.isolated_filesystem():
@@ -229,7 +229,7 @@ def test_sync_quiet(tmpdir):
 
 def test_editable_package(tmpdir):
     """ piptools can compile an editable """
-    fake_package_dir = os.path.join(os.path.split(__file__)[0], 'fixtures', 'small_fake_package')
+    fake_package_dir = os.path.join(os.path.split(__file__)[0], 'test_data', 'small_fake_package')
     fake_package_dir = 'file:' + pathname2url(fake_package_dir)
     runner = CliRunner()
     with runner.isolated_filesystem():
@@ -265,7 +265,7 @@ def test_locally_available_editable_package_is_not_archived_in_cache_dir(tmpdir)
     """ piptools will not create an archive for a locally available editable requirement """
     cache_dir = tmpdir.mkdir('cache_dir')
 
-    fake_package_dir = os.path.join(os.path.split(__file__)[0], 'fixtures', 'small_fake_package')
+    fake_package_dir = os.path.join(os.path.split(__file__)[0], 'test_data', 'small_fake_package')
     fake_package_dir = 'file:' + pathname2url(fake_package_dir)
 
     with mock.patch('piptools.repositories.pypi.CACHE_DIR', new=str(cache_dir)):
@@ -305,7 +305,7 @@ def test_upgrade_packages_option(tmpdir):
     """
     piptools respects --upgrade-package/-P inline list.
     """
-    fake_package_dir = os.path.join(os.path.split(__file__)[0], 'fixtures', 'minimal_wheels')
+    fake_package_dir = os.path.join(os.path.split(__file__)[0], 'test_data', 'minimal_wheels')
     runner = CliRunner()
     with runner.isolated_filesystem():
         with open('requirements.in', 'w') as req_in:
@@ -325,7 +325,7 @@ def test_upgrade_packages_option(tmpdir):
 
 def test_generate_hashes_with_editable():
     small_fake_package_dir = os.path.join(
-        os.path.split(__file__)[0], 'fixtures', 'small_fake_package')
+        os.path.split(__file__)[0], 'test_data', 'small_fake_package')
     small_fake_package_url = 'file:' + pathname2url(small_fake_package_dir)
     runner = CliRunner()
     with runner.isolated_filesystem():
