4b9d14046ed89a587c976ac871ff9af3d67bc1a3
jbergknoff-10e <jonathan@10etechnology.com>
2018-03-15 09:25:30 -0700

    Option to restrict attention to user directory

--- piptools/scripts/sync.py
+++ piptools/scripts/sync.py
@@ -27,8 +27,9 @@ DEFAULT_REQUIREMENTS_FILE = 'requirements.txt'
 @click.option('--extra-index-url', multiple=True, help="Add additional index URL to search", envvar='PIP_EXTRA_INDEX_URL')  # noqa
 @click.option('--no-index', is_flag=True, help="Ignore package index (only looking at --find-links URLs instead)")
 @click.option('-q', '--quiet', default=False, is_flag=True, help="Give less output")
+@click.option('--user-only', is_flag=True, help="Restrict attention to user directory")
 @click.argument('src_files', required=False, type=click.Path(exists=True), nargs=-1)
-def cli(dry_run, force, find_links, index_url, extra_index_url, no_index, quiet, src_files):
+def cli(dry_run, force, find_links, index_url, extra_index_url, no_index, quiet, user_only, src_files):
     """Synchronize virtual environment with requirements.txt."""
     if not src_files:
         if os.path.exists(DEFAULT_REQUIREMENTS_FILE):
@@ -56,7 +57,7 @@ def cli(dry_run, force, find_links, index_url, extra_index_url, no_index, quiet,
         log.error(str(e))
         sys.exit(2)
 
-    installed_dists = pip.get_installed_distributions(skip=[])
+    installed_dists = pip.get_installed_distributions(skip=[], user_only=user_only)
     to_install, to_uninstall = sync.diff(requirements, installed_dists)
 
     install_flags = []
