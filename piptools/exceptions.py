cc8613102f0a038ea4c45e8ee2f4feabe0f657ec
Rick van Hattem <Wolph@wol.ph>
2018-04-25 03:46:08 +0200

    Added clearer error reporting when skipping pre-releases

--- piptools/exceptions.py
+++ piptools/exceptions.py
@@ -9,14 +9,31 @@ class NoCandidateFound(PipToolsError):
         self.index_urls = index_urls
 
     def __str__(self):
-        sorted_versions = sorted(c.version for c in self.candidates_tried)
+        sorted_candidates = sorted(self.candidates_tried)
+        versions = []
+        pre_versions = []
+
+        for candidate in sorted(self.candidates_tried):
+            version = str(candidate.version)
+            if candidate.version.is_prerelease:
+                pre_versions.append(version)
+            else:
+                versions.append(version)
+
         lines = [
             'Could not find a version that matches {}'.format(self.ireq),
-            'Tried: {}'.format(', '.join(str(version) for version in sorted_versions) or '(no version found at all)')
         ]
-        if sorted_versions:
+
+        if versions:
+            lines.append('Tried: {}'.format(', '.join(versions)))
+
+        if pre_versions:
+            lines.append('Skipped pre-versions: {}'.format(', '.join(pre_versions)))
+
+        if versions or pre_versions:
             lines.append('There are incompatible versions in the resolved dependencies.')
         else:
+            lines.append('No versions found')
             lines.append('{} {} reachable?'.format(
                 'Were' if len(self.index_urls) > 1 else 'Was', ' or '.join(self.index_urls))
             )
