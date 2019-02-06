fb646cbe0be77ccddc031718ff64bba18415c8c0
Albert Tugushev <albert@tugushev.ru>
2019-02-07 02:00:44 +0300

    Fix the output file for pip-compile with an explicit setup.py as source file
    The command `pip-compile setup.py` generates a `setup.txt`, but
    it should be a requirements.txt file.

--- piptools/scripts/compile.py
+++ piptools/scripts/compile.py
@@ -21,6 +21,7 @@ from ..utils import (dedup, is_pinned_requirement, key_from_req, UNSAFE_PACKAGES
 from ..writer import OutputWriter
 
 DEFAULT_REQUIREMENTS_FILE = 'requirements.in'
+DEFAULT_REQUIREMENTS_OUTPUT_FILE = 'requirements.txt'
 
 
 @click.command()
@@ -72,15 +73,14 @@ def cli(verbose, quiet, dry_run, pre, rebuild, find_links, index_url, extra_inde
             src_files = (DEFAULT_REQUIREMENTS_FILE,)
         elif os.path.exists('setup.py'):
             src_files = ('setup.py',)
-            if not output_file:
-                output_file = 'requirements.txt'
         else:
             raise click.BadParameter(("If you do not specify an input file, "
                                       "the default is {} or setup.py").format(DEFAULT_REQUIREMENTS_FILE))
 
-    if len(src_files) == 1 and src_files[0] == '-':
-        if not output_file:
-            raise click.BadParameter('--output-file is required if input is from stdin')
+    if src_files == ('-',) and not output_file:
+        raise click.BadParameter('--output-file is required if input is from stdin')
+    elif src_files == ('setup.py',):
+        output_file = DEFAULT_REQUIREMENTS_OUTPUT_FILE
 
     if len(src_files) > 1 and not output_file:
         raise click.BadParameter('--output-file is required if two or more input files are given.')
