76a5f1139f9f5d147d574beff1db595028c6e7c2
George Kettleborough <kettleg@gmail.com>
2019-06-07 20:39:43 +0100

    Output all logging to stderr (#353)

--- piptools/logging.py
+++ piptools/logging.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import sys
-
 from . import click
 
 
@@ -11,6 +9,7 @@ class LogContext(object):
         self.verbosity = verbosity
 
     def log(self, *args, **kwargs):
+        kwargs.setdefault("err", True)
         click.secho(*args, **kwargs)
 
     def debug(self, *args, **kwargs):
@@ -23,12 +22,10 @@ class LogContext(object):
 
     def warning(self, *args, **kwargs):
         kwargs.setdefault("fg", "yellow")
-        kwargs.setdefault("file", sys.stderr)
         self.log(*args, **kwargs)
 
     def error(self, *args, **kwargs):
         kwargs.setdefault("fg", "red")
-        kwargs.setdefault("file", sys.stderr)
         self.log(*args, **kwargs)
 
 
