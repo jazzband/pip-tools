bb61e0828aca31518b78e0df0c2b834ece96a23d
Hasan Ramezani <hasan.r67@gmail.com>
2019-11-22 18:19:29 +0100

    Moved global test constants to tests.constants module (#996)

---
+++ tests/constants.py
@@ -0,0 +1,5 @@
+import os
+
+TEST_DATA_PATH = os.path.join(os.path.split(__file__)[0], "test_data")
+MINIMAL_WHEELS_PATH = os.path.join(TEST_DATA_PATH, "minimal_wheels")
+PACKAGES_PATH = os.path.join(TEST_DATA_PATH, "packages")
