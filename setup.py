5f35f82b70844174a075bce8fb88c442807427d0
Albert Tugushev <albert@tugushev.ru>
2020-02-04 01:52:24 +0700

    Remove old pips from CIs

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     package_data={},
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     setup_requires=["setuptools_scm"],
-    install_requires=["click>=7", "six"],
+    install_requires=["click>=7", "six", "pip>=20.0"],
     zip_safe=False,
     entry_points={
         "console_scripts": [
