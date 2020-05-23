81241234dc034c3fecac38576e53e8b0314f04ea
Albert Tugushev <albert@tugushev.ru>
2020-05-23 18:07:44 +0700

    Convert path to str explicitly

--- tests/conftest.py
+++ tests/conftest.py
@@ -274,7 +274,7 @@ def run_setup_file():
     def _run_setup_file(package_dir_path, *args):
         setup_file = str(package_dir_path / "setup.py")
         return check_call(
-            (sys.executable, setup_file) + args, cwd=package_dir_path
+            (sys.executable, setup_file) + args, cwd=str(package_dir_path)
         )  # nosec
 
     return _run_setup_file
