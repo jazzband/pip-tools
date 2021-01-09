8e2f3c102d7674aed59e559f10885607f533e646
Stefan Sullivan <stefan.sullivan@gmail.com>
2021-01-08 18:21:52 -0800

    add pipfile test for editables

--- tests/test_compat_pipfile.py
+++ tests/test_compat_pipfile.py
@@ -2,6 +2,7 @@ from optparse import Values
 
 import pytest
 from pip._internal.req.req_file import ParsedLine
+from pip._internal.req.req_file import get_line_parser
 from pip._internal.req.req_file import handle_requirement_line
 
 from piptools._compat.pipfile_compat import _handle_requirement
@@ -27,6 +28,24 @@ def test_pipfile_requirement(pipfile_requirement, requirement_as_text):
     expected_requirement = ParsedLine(filename, line, requirement_as_text, Values(), False)
 
     expected_requirement = handle_requirement_line(expected_requirement)
+    parsed_requirement = _handle_requirement(pipfile_requirement, filename=filename, lineno=line)
+
+    assert requirement_to_dict(expected_requirement) == requirement_to_dict(parsed_requirement)
+
+@pytest.mark.parametrize(
+    "pipfile_requirement,requirement_as_text",
+    [
+        (('req_name', dict(editable=True, path="/path/to/req")), "-e /path/to/req"),
+    ]
+)
+def test_pipfile_requirement_editable(pipfile_requirement, requirement_as_text):
+    line_parser = get_line_parser(None)
+
+    filename = "no file"
+    line = 0
+    args, opts = line_parser(requirement_as_text)
+
+    expected_requirement = handle_requirement_line(ParsedLine(filename, line, args, opts, False))
     parsed_requirement = _handle_requirement(pipfile_requirement, filename=filename, lineno=line)
 
     assert requirement_to_dict(expected_requirement) == requirement_to_dict(parsed_requirement)
