462fb9cd41348e38de5f19d232c4fe5ba2be8602
Nico Albers <nico.albers@aboutyou.com>
2021-05-27 17:14:49 +0200

    name variable corresponding to click major version properly

--- piptools/_compat/click_compat.py
+++ piptools/_compat/click_compat.py
@@ -1,5 +1,5 @@
 import click
 from pip._vendor.packaging.version import Version
 
-CLICK_VERSION = Version(click.__version__).major
-IS_CLICK_VER_8_PLUS = CLICK_VERSION > 7
+CLICK_MAJOR_VERSION = Version(click.__version__).major
+IS_CLICK_VER_8_PLUS = CLICK_MAJOR_VERSION > 7
