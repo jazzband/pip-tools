7bbd4bcddece0a40605686a338975184f044dac7
Adam Sołtysik <adam27.sol@gmail.com>
2021-06-12 23:02:21 +0200

    Improve test to ensure case insensitivity

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -352,18 +352,16 @@ def test_write_order(writer, from_line):
     """
     writer.emit_header = False
 
-    lines = writer._iter_lines(
-        [
-            from_line("package_a==0.1"),
-            from_line("package-b==2.3.4"),
-            from_line("package==5.6"),
-            from_line("package2==7.8.9"),
-        ]
-    )
+    packages = [
+        from_line("package_a==0.1"),
+        from_line("Package-b==2.3.4"),
+        from_line("Package==5.6"),
+        from_line("package2==7.8.9"),
+    ]
     expected_lines = [
         "package==5.6",
         "package_a==0.1",
         "package-b==2.3.4",
         "package2==7.8.9",
     ]
-    assert list(lines) == expected_lines
+    assert list(writer._iter_lines(packages)) == expected_lines
