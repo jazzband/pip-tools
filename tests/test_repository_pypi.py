14afbf815066f7036e50bf4a718307b220f4b4b5
Patrick Lannigan <p.lannigan@gmail.com>
2021-08-02 09:46:26 -0400

    Change log level of hash message (#1460)

--- tests/test_repository_pypi.py
+++ tests/test_repository_pypi.py
@@ -22,10 +22,7 @@ def test_generate_hashes_all_platforms(capsys, pip_conf, from_line, pypi_reposit
         assert pypi_repository.get_hashes(ireq) == expected
     captured = capsys.readouterr()
     assert captured.out == ""
-    assert (
-        captured.err.strip()
-        == "Couldn't get hashes from PyPI, fallback to hashing files"
-    )
+    assert captured.err == ""
 
 
 @pytest.mark.network
