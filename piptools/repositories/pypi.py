5528414fa51ae5ec739b743a36586695f9f36c1c
Patrick Lannigan <p.lannigan@gmail.com>
2021-10-04 08:53:47 -0400

    One last match -> matching

--- piptools/repositories/pypi.py
+++ piptools/repositories/pypi.py
@@ -306,7 +306,7 @@ class PyPIRepository(BaseRepository):
         """
         Given a pinned InstallRequirement, return a set of hashes that can be used to verify the
         file to install for the requirement. If single_hash is True, the set will only have the
-        hash for the best match file to install based on the current execution environment. When
+        hash for the best matching file to install based on the current execution environment. When
         False (the default), include hashes for all of the files for a given requirement.
 
         Files that are unhashable are excluded from the returned set.
