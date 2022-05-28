9b29e2f454d254bed56f67903693f340793e5815
Richard Frank <richafrank@users.noreply.github.com>
2022-05-27 20:15:38 -0400

    Support for the importlib.metadata metadata implementation
    enabled by pip 22.1 by default on Python 3.11 (or later)

--- piptools/_compat/pip_compat.py
+++ piptools/_compat/pip_compat.py
@@ -1,5 +1,5 @@
 import optparse
-from typing import Iterator, Optional
+from typing import Callable, Iterator, Optional, cast
 
 import pip
 from pip._internal.index.package_finder import PackageFinder
@@ -8,6 +8,7 @@ from pip._internal.req import InstallRequirement
 from pip._internal.req import parse_requirements as _parse_requirements
 from pip._internal.req.constructors import install_req_from_parsed_requirement
 from pip._vendor.packaging.version import parse as parse_version
+from pip._vendor.pkg_resources import Requirement
 
 PIP_VERSION = tuple(map(int, parse_version(pip.__version__).base_version.split(".")))
 
@@ -15,6 +16,9 @@ PIP_VERSION = tuple(map(int, parse_version(pip.__version__).base_version.split("
 __all__ = [
     "get_build_tracker",
     "update_env_context_manager",
+    "dist_requires",
+    "uses_pkg_resources",
+    "Distribution",
 ]
 
 
@@ -42,3 +46,39 @@ else:
         get_build_tracker,
         update_env_context_manager,
     )
+
+
+# The Distribution interface has changed between pkg_resources and
+# importlib.metadata, so this compat layer allows for a consistent access
+# pattern. In pip 22.1, importlib.metdata became the default on Python 3.11
+# (and later), but is overrideable. `select_backend` returns what's being used.
+
+
+def _uses_pkg_resources() -> bool:
+
+    if PIP_VERSION[:2] < (22, 1):
+        return True
+    else:
+        from pip._internal.metadata import select_backend
+        from pip._internal.metadata.pkg_resources import Distribution as _Dist
+
+        return select_backend().Distribution is _Dist
+
+
+uses_pkg_resources = _uses_pkg_resources()
+
+if uses_pkg_resources:
+    from operator import methodcaller
+
+    from pip._vendor.pkg_resources import Distribution
+
+    dist_requires = cast(
+        Callable[[Distribution], Iterator[Requirement]], methodcaller("requires")
+    )
+else:
+    from pip._internal.metadata import select_backend
+
+    Distribution = select_backend().Distribution
+
+    def dist_requires(dist: "Distribution") -> Iterator[Requirement]:
+        return map(Requirement, dist.requires or ())
