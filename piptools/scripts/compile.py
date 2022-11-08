e8ec6576b3b89f9eb7f9d8a3111658d4c53e1ef2
Maximilian Friedersdorff <maximilian.friedersdorff@envsys.co.uk>
2022-11-08 10:40:27 +0000

    Prompt to use the backtracking resolver

--- piptools/scripts/compile.py
+++ piptools/scripts/compile.py
@@ -16,7 +16,7 @@ from pip._internal.utils.misc import redact_auth_from_url
 
 from .._compat import IS_CLICK_VER_8_PLUS, parse_requirements
 from ..cache import DependencyCache
-from ..exceptions import PipToolsError
+from ..exceptions import PipToolsError, NoCandidateFound
 from ..locations import CACHE_DIR
 from ..logging import log
 from ..repositories import LocalRequirementsRepository, PyPIRepository
@@ -545,6 +545,12 @@ def cli(
         )
         results = resolver.resolve(max_rounds=max_rounds)
         hashes = resolver.resolve_hashes(results) if generate_hashes else None
+    except NoCandidateFound as e:
+        if resolver_cls == LegacyResolver:
+            log.error("Using legacy resolver.  Consider using backtracking resolver with --resolver=backtracking")
+
+        log.error(str(e))
+        sys.exit(2)
     except PipToolsError as e:
         log.error(str(e))
         sys.exit(2)
