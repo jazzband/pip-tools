07f72f8014adb693fe6384d95dd45d2b6fac1d46
q0w <43147888+q0w@users.noreply.github.com>
2022-12-29 03:15:46 +0300

    Sort imports

--- piptools/_compat/__init__.py
+++ piptools/_compat/__init__.py
@@ -2,4 +2,4 @@ from __future__ import annotations
 
 from .pip_compat import PIP_VERSION, install_req_from_line, parse_requirements
 
-__all__ = ["PIP_VERSION", "parse_requirements", "install_req_from_line"]
+__all__ = ["PIP_VERSION", "install_req_from_line", "parse_requirements"]
