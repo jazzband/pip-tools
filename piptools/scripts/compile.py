f1304aa41031c0eb96d7174de40148dcf500a3c7
Ryan Hiebert <ryan@ryanhiebert.com>
2023-08-06 14:33:08 +0000

    Warn about strip extras by default

--- piptools/scripts/compile.py
+++ piptools/scripts/compile.py
@@ -226,9 +226,9 @@ def _determine_linesep(
     ),
 )
 @click.option(
-    "--strip-extras",
+    "--strip-extras/--no-strip-extras",
     is_flag=True,
-    default=False,
+    default=None,
     help="Assure output file is constraints compatible, avoiding use of extras.",
 )
 @click.option(
@@ -365,7 +365,7 @@ def cli(
     output_file: LazyFile | IO[Any] | None,
     newline: str,
     allow_unsafe: bool,
-    strip_extras: bool,
+    strip_extras: bool | None,
     generate_hashes: bool,
     reuse_hashes: bool,
     src_files: tuple[str, ...],
@@ -675,6 +675,15 @@ def cli(
     linesep = _determine_linesep(
         strategy=newline, filenames=(output_file.name, *src_files)
     )
+
+    if strip_extras is None:
+        strip_extras = False
+        log.warning(
+            "WARNING: --strip-extras is becoming the default. "
+            "To silence this warning, "
+            "either use --strip-extras to opt into the new default "
+            "or use --no-strip-extras to retain the existing behavior."
+        )
 
     ##
     # Output
