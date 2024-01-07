1d4c02eb583bf73325d9adbd2e1011f06d6f61d2
hauntsaninja <hauntsaninja@gmail.com>
2024-01-07 01:56:29 -0800

    fix on windows

--- piptools/build.py
+++ piptools/build.py
@@ -100,7 +100,7 @@ def maybe_statically_parse_project_metadata(
             if requirement.name == package_name:
                 # Similar to logic for handling self-referential requirements
                 # from _prepare_requirements
-                requirement.url = f"file://{src_file.parent}"
+                requirement.url = f"file://{src_file.parent.as_posix()}"
             # Note we don't need to modify `requirement` to include this extra
             marker = Marker(f"extra == '{extra}'")
             install_requirements.append(
