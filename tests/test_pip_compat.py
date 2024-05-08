3a9302ed977be9d55ef588a5797596e9b04e19cf
Micah Denbraver <macro@helena.local>
2024-05-08 12:36:43 -0700

    use pytest's `tmp_path`
    for more information, see https://pre-commit.ci

--- tests/test_pip_compat.py
+++ tests/test_pip_compat.py
@@ -12,21 +12,18 @@ from piptools.repositories import PyPIRepository
 from .constants import PACKAGES_RELATIVE_PATH
 
 
-def test_parse_requirements_preserve_editable_relative_path(repository):
+def test_parse_requirements_preserve_editable_relative_path(tmp_path, repository):
     test_package_path = str(
         PurePosixPath(Path(PACKAGES_RELATIVE_PATH)) / "small_fake_a"
     )
+    requirements_in_path = str(tmp_path / "requirements.in")
 
-    infile = tempfile.NamedTemporaryFile("w", delete=False)
-    try:
-        infile.write(f"-e {test_package_path}")
-        infile.close()
+    with open(requirements_in_path, 'w') as requirements_in_file:
+        requirements_in_file.write(f"-e {test_package_path}")
 
-        [install_requirement] = parse_requirements(
-            infile.name, session=repository.session
-        )
-    finally:
-        os.unlink(infile.name)
+    [install_requirement] = parse_requirements(
+        requirements_in_path, session=repository.session
+    )
 
     assert install_requirement.link.url == test_package_path
     assert install_requirement.link.file_path == test_package_path
