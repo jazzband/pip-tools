c66ea7863d7bd6b075856fd2ad16979a18e3cd49
Stephen Rosen <sirosen@globus.org>
2025-03-28 00:10:47 -0500

    Add `--group` to `pip-compile` options
    Define the option, but don't implement any behavior other than parsing
    yet.
    
    This is defined very similarly to `--group` for `pip` commands, but
    allows for a double-colon as a delimiter, rather than requiring
    only one.

--- piptools/scripts/compile.py
+++ piptools/scripts/compile.py
@@ -127,6 +127,7 @@ Examples:
 @options.reuse_hashes
 @options.max_rounds
 @options.src_files
+@options.group
 @options.build_isolation
 @options.emit_find_links
 @options.cache_dir
@@ -171,6 +172,7 @@ def cli(
     generate_hashes: bool,
     reuse_hashes: bool,
     src_files: tuple[str, ...],
+    group: tuple[tuple[str, str], ...],
     max_rounds: int,
     build_isolation: bool,
     emit_find_links: bool,
