d7116c5f970d0f1995de6288f7391061a8ea85da
Stephen Rosen <sirosen0@gmail.com>
2026-08-14 19:17:15 -0500

    Add docstrings to help opt tests

--- tests/unit/scripts/test_options.py
+++ tests/unit/scripts/test_options.py
@@ -8,6 +8,10 @@ from piptools.scripts.options import help_option
 
 
 def test_help_opt_no_epilog(runner: CliRunner) -> None:
+    """
+    Test that the customized ``--help`` option can be used without an epilog declared.
+    """
+
     @click.command("my-command")
     @help_option()
     def my_command() -> None:
@@ -21,6 +25,11 @@ def test_help_opt_no_epilog(runner: CliRunner) -> None:
 
 
 def test_help_opt_with_epilog(runner: CliRunner) -> None:
+    """
+    Test that the ``epilog`` option for customized ``--help`` test appends the given
+    text to the end of the helptext.
+    """
+
     @click.command("my-command")
     @help_option(epilog="hello there")
     def my_command() -> None:
